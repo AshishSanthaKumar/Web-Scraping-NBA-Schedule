1f6010aab0258af75752787a8a952608b856427f
Ashish Santha Kumar <57326393+AshishSanthaKumar@users.noreply.github.com>
2022-09-23 20:06:05 -0700

    Schedule pulled in

--- scrapping.py
+++ scrapping.py
@@ -1,16 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+from openpyxl import Workbook
 
-url = 'https://www.basketball-reference.com/leagues/NBA_2023_games-october.html'
-response = requests.get(url)
-html = response.text
+#Defining the workbook and column headers 
+wb = Workbook()
+ws = wb.active
 
-soup = BeautifulSoup(html, "html.parser")
+ws['A1'] = "Match Day"
+ws['B1'] = "Match Time"
+ws['C1'] = "Visitor Team"
+ws['D1'] = "Home Team"
+ws['E1'] = "Arena"
 
-october = soup.find('tbody')
-match = october.find('tr')
-match_day = match.find('th').text
-match_time = match.find('td').text
-visiting_team = match.find('td', class_='left').text
+#Iterating through URL
 
-print(match_day, match_time, visiting_team)
+months = ['october','november','december','january','february','march','april']
+for month in months:
+    
+    url = f"https://www.basketball-reference.com/leagues/NBA_2023_games-{month}.html"
+    response = requests.get(url)
+    html = response.text
+      
+    #reading the contents of each URL
+    soup = BeautifulSoup(html, "html.parser")
+    
+    Schedule = soup.find('tbody')
+    
+    for match in Schedule.find_all('tr'):
+        match_day = match.find('th').text
+        match_time = match.find('td').text
+        list1 = [match_day, match_time]
+        for td in match.find_all('td', class_='left'):
+            list1.append(td.text)
+            
+        ws.append(list1)
+
+
+# Save the file
+wb.save("sample.xlsx")
